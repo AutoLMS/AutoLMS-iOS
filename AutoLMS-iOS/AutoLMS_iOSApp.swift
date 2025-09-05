7e5b2686c3e161dd9884f9949d09f5783eba77c5
Choi Jiwoo <96974725+elaus00@users.noreply.github.com>
2025-09-05 20:47:38 +0900

    Initial Commit

---
+++ AutoLMS-iOS/AutoLMS_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLMS_iOSApp.swift
+//  AutoLMS-iOS
+//
+//  Created by elaus on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoLMS_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoLMS-iOSTests/AutoLMS_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLMS_iOSTests.swift
+//  AutoLMS-iOSTests
+//
+//  Created by elaus on 9/5/25.
+//
+
+import Testing
+@testable import AutoLMS_iOS
+
+struct AutoLMS_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
